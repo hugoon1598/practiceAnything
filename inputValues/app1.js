fe48c6604d7016b2625b680ddace56635a18cfd3
hugoon1598 <154963097+hugoon1598@users.noreply.github.com>
2024-01-28 23:23:37 +0900

    task4
    마지막 코드 추가.
    이렇게 함으로써 이벤트가 발생해서 submit 되는 것을 막을 수 잇음
    
    function onSubmit(event) {
        event.preventDefault();
        console.log(loginInput.value);
        };

--- inputValues/app1.js
+++ inputValues/app1.js
@@ -2,9 +2,9 @@ const loginForm =document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 
 
-function onSubmit(tomato) {
-    tomato.preventDefault(tomato);
-    console.log(tomato);
+function onSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
     };
 
 loginForm.addEventListener("submit", onSubmit);
