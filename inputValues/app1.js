670a054e01e7b48f2b1113aea16ad9b037fd014d
hugoon1598 <154963097+hugoon1598@users.noreply.github.com>
2024-01-28 22:50:37 +0900

    task2
    create a "preventDefault"
    
    practice to a "tomato"

--- inputValues/app1.js
+++ inputValues/app1.js
@@ -2,9 +2,9 @@ const loginForm =document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 
 
-function onSubmit() {
-    const username = loginInput.value;
-    console.log(username);
+function onSubmit(tomato) {
+    tomato.preventDefault(tomato);
+    console.log(tomato);
     };
 
 loginForm.addEventListener("submit", onSubmit);
