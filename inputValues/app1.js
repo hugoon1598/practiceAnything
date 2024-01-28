cede766df093b08c6d94834749f31ee49acec9c2
hugoon1598 <154963097+hugoon1598@users.noreply.github.com>
2024-01-28 16:35:12 +0900

    FornSubmission
    main인 지난시간에 이어 이번시간은 condition으로 콘솔에 값을 받는 것과
    궁극적으로는 conditon을 사용하지 않고 html의 도움을 받으며 콘솔로 값을 받아오는 것까지  학습.

--- inputValues/app1.js
+++ inputValues/app1.js
@@ -2,9 +2,9 @@ const loginInput = document.querySelector("#login-form input")
 const loginButton= document.querySelector("#login-form button")
 
 function onLoginBtnClick() {
-    console.log(loginInput. value);
-    console.log("click!!!");
-}
+    const username = loginInput.value;
+    console.log(username);
+    };
 
 loginButton.addEventListener("click", onLoginBtnClick);
 
