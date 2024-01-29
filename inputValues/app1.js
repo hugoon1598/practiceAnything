8af34d6c8b631919483ab4f947af69cbe2c31826
hugoon1598 <154963097+hugoon1598@users.noreply.github.com>
2024-01-29 15:51:34 +0900

    2
    const link, function, addEventListener
    
    html에서 링크태그 작동되게 수정,
    JS에서 link라는 변수생성하고 onClick기능을 생성하고 이벤트리스너 만들어줌

--- inputValues/app1.js
+++ inputValues/app1.js
@@ -1,11 +1,16 @@
 const loginForm =document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 
+const link = document.querySelector("a")
 
 function onSubmit(event) {
     event.preventDefault();
     console.log(loginInput.value);
     };
 
-loginForm.addEventListener("submit", onSubmit);
+function onClick(){
+    alert("click!")
+}
 
+loginForm.addEventListener("submit", onSubmit);
+link.addEventListener("click", onClick);
