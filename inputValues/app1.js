a17e3ebdf89b3c83293b195fb86d976ddb0b6709
hugoon1598 <154963097+hugoon1598@users.noreply.github.com>
2024-02-01 18:38:36 +0900

    Final
    만약 local storage에 유저정보가 없다면, 우리는 form의 submit를 기다리고
    form이 submit되면 우리는 input으로부터 유저정보를 받고
    input에서 받은 user를 가진 paintgreetings를 호출

--- inputValues/app1.js
+++ inputValues/app1.js
@@ -2,16 +2,15 @@ const loginForm =document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 const greeting = document.querySelector("#greeting")
 
-const HIDDEN_CLASS = "hidden";
+const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY="username";
 
 function onSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASS);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username=loginInput.value;
     localStorage.setItem("USERNAME_KEY", username)
-    greeting.classList.remove(HIDDEN_CLASS);
-    greeting.innerText=`Hello! ${username} This is the testing`;
+    paintGreetings(username);
     console.log(username);
     };
 
@@ -21,14 +20,19 @@ function handleLinkClick(event){
     
 }
 
-loginForm.addEventListener("submit", onSubmit);
+function paintGreetings(username){
+    greeting.innerText=`Hello! ${username} This is the testing`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 
 const savedUsername = localStorage.getItem("USERNAME_KEY");
 
 console.log(savedUsername);
 
 if(savedUsername === null) {
-    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onSubmit);
 } else{
-    //show the greetings
+    paintGreetings(savedUsername);
 }
