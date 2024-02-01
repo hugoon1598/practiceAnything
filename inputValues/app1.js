a991586409cb080ad91be2d7fdf9d294b047c14f
hugoon1598 <154963097+hugoon1598@users.noreply.github.com>
2024-02-01 17:41:11 +0900

    one step

--- inputValues/app1.js
+++ inputValues/app1.js
@@ -1,13 +1,15 @@
 const loginForm =document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 const greeting = document.querySelector("#greeting")
+
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY="username";
 
 function onSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASS);
     const username=loginInput.value;
-    localStorage.setItem("username", username)
+    localStorage.setItem("USERNAME_KEY", username)
     greeting.classList.remove(HIDDEN_CLASS);
     greeting.innerText=`Hello! ${username} This is the testing`;
     console.log(username);
@@ -20,3 +22,13 @@ function handleLinkClick(event){
 }
 
 loginForm.addEventListener("submit", onSubmit);
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+console.log(savedUsername);
+
+if(savedUsername === null) {
+    //show the form
+} else{
+    //show the greetings
+}
